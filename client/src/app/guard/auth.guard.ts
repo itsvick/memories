4ffff9efceb2232c50a265e66f6ba6bc89abd0e6
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-12-12 01:59:48 +0530

    Added frontend

---
+++ client/src/app/guard/auth.guard.ts
@@ -0,0 +1,15 @@
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(public router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (localStorage.getItem('currentUser')) {
+            return true;
+        }
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
